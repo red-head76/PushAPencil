28b35d6187a2a6c036e9867953b02610bc357d2d
red-head76 <reddy.uni@t-online.de>
2021-02-11 18:45:52 +0100

    bucket tool comments and alternative implementation

--- drawToCanvas.js
+++ drawToCanvas.js
@@ -113,6 +113,78 @@ function endLine(e) {
         }
     }
 }
+function bucketTool2(e) {
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+    colorLayer = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+    startX = getX(e);
+    startY = getY(e);
+    pixelStack = [[startX, startY]];
+
+    function pixelPos(x, y) {   // converting x, y coordinates into 1d position
+        return ((y * canvasWidth + x) * 4);
+    }
+
+    var StartR = colorLayer.data[pixelPos(startX, startY)];
+    var StartG = colorLayer.data[pixelPos(startX, startY) + 1];
+    var StartB = colorLayer.data[pixelPos(startX, startY) + 2];
+    var StartA = colorLayer.data[pixelPos(startX, startY) + 3];
+    var FillColorRGB = HEXtoRGB(ctx.strokeStyle);
+    var newPos, x, y;
+
+    while (pixelStack.length) {
+        newPos = pixelStack.pop();
+        x = newPos[0];
+        y = newPos[1];
+
+        if (matchStartColor(pixelPos(x, y)) && (StartR !== FillColorRGB[0] ||
+            StartG !== FillColorRGB[1] || StartB !== FillColorRGB[2])) {
+            colorPixel(pixelPos(x, y));
+
+            pixelStack.push([x, y + 1]); // below
+            pixelStack.push([x, y - 1]); // above
+            pixelStack.push([x + 1, y]); // right
+            pixelStack.push([x - 1, y]); // left
+        }
+    }
+    ctx.putImageData(colorLayer, 0, 0);
+
+        function matchStartColor(pixelPos) {
+        var r = colorLayer.data[pixelPos];
+        var g = colorLayer.data[pixelPos + 1];
+        var b = colorLayer.data[pixelPos + 2];
+        var a = colorLayer.data[pixelPos + 3];
+
+        return (r == StartR && g == StartG && b == StartB && a==StartA);
+    }
+
+    function HEXtoRGB(hex) {
+        hex = hex.replace(/#/g, '');
+        if (hex.length === 3) {
+            hex = hex.split('').map(function (hex) {
+                return hex + hex;
+            }).join('');
+        }
+        var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+        if (result) {
+            var red = parseInt(result[1], 16);
+            var green = parseInt(result[2], 16);
+            var blue = parseInt(result[3], 16);
+
+            return [red, green, blue];
+        } else {
+            return null;
+        }
+    }
+
+    function colorPixel(pixelPos) {
+        var FillColorRGB = HEXtoRGB(ctx.strokeStyle);
+        colorLayer.data[pixelPos] = FillColorRGB[0];
+        colorLayer.data[pixelPos+1] = FillColorRGB[1];
+        colorLayer.data[pixelPos+2] = FillColorRGB[2];
+        colorLayer.data[pixelPos+3] = 255;
+    }
+}
 
 // bucket tool (fill tool) from
 // http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
@@ -150,6 +222,8 @@ function bucketTool(e) {
             while (y < canvasHeight-1 && matchStartColor(pixelPos(x, y))) {
                 colorPixel(pixelPos(x, y));
 
+                // Look to the left if there is still a pixel to be colored and at it
+                // to the stack in that case
                 if (x > 0) {
                     if(matchStartColor(pixelPos(x, y) - 4)) {
                         if(!reachLeft) {
@@ -160,6 +234,7 @@ function bucketTool(e) {
                         reachLeft = false;
                     }
                 }
+                // Look to the right
                 if (x < canvasWidth - 1) {
                     if (matchStartColor(pixelPos(x, y) + 4)) {
                         if(!reachRight) {
@@ -176,8 +251,7 @@ function bucketTool(e) {
         }
     }
 
-    function matchStartColor(pixelPos)
-    {
+    function matchStartColor(pixelPos) {
         var r = colorLayer.data[pixelPos];
         var g = colorLayer.data[pixelPos + 1];
         var b = colorLayer.data[pixelPos + 2];
@@ -205,8 +279,7 @@ function bucketTool(e) {
         }
     }
 
-    function colorPixel(pixelPos)
-    {
+    function colorPixel(pixelPos) {
         var FillColorRGB = HEXtoRGB(ctx.strokeStyle);
         colorLayer.data[pixelPos] = FillColorRGB[0];
         colorLayer.data[pixelPos+1] = FillColorRGB[1];
