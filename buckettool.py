b0e50c6ccb349fe97710c03a974dbe1ab6097aff
red-head76 <reddy.uni@t-online.de>
2021-02-11 17:47:53 +0100

    buckettool py zum testen des fill algos mit python

---
+++ buckettool.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from queue import Queue
+
+cWidth = 100
+cHeight = 100
+colorLayer = np.ones((cWidth, cHeight)) * 3
+
+startPixel = [50, 50]
+
+DesiredValue = 3
+
+colorLayer[np.arange(40, 60), 40] = 2
+colorLayer[np.arange(40, 60), 60] = 2
+colorLayer[40, np.arange(40, 60)] = 2
+colorLayer[59, np.arange(40, 60)] = 2
+for i in range(40, 60):
+    for j in range(40, 60):
+        if ((i - 50)**2 + (j - 50)**2) > 80:
+            colorLayer[i, j] = 2
+
+
+def BucketTool(StartPixel, DesiredValue):
+    pixelStack = Queue()
+    pixelStack.put(startPixel)
+    print("Pixelstack", pixelStack.qsize())
+    StartValue = colorLayer[startPixel[0], startPixel[1]]
+    if (StartValue != DesiredValue):
+        while (pixelStack.qsize()):
+            newPos = pixelStack.get()
+            x = newPos[0]
+            y = newPos[1]
+            while (y >= 0 and matchStartValue(x, y, StartValue)):
+                y -= 1
+            y += 1
+            reachLeft = False
+            reachRight = False
+
+            while (y < cHeight - 1 and matchStartValue(x, y, StartValue)):
+                setPixel(x, y)
+                if (x > 0):
+                    if matchStartValue(x - 1, y, StartValue) and not reachLeft:
+                        pixelStack.put([x - 1, y])
+                        colorLayer[x - 1, y] = 1
+                        reachLeft = True
+                    elif reachLeft:
+                        reachLeft = False
+                if (x < cWidth - 1):
+                    if matchStartValue(x + 1, y, StartValue) and not reachRight:
+                        pixelStack.put([x + 1, y])
+                        colorLayer[x + 1, y] = 1
+                        reachRight = True
+                    elif reachRight:
+                        reachRight = False
+                y += 1
+
+
+def matchStartValue(x, y, StartValue):
+    return (colorLayer[x, y] == StartValue or colorLayer[x, y] == 1)
+
+
+def setPixel(x, y):
+    colorLayer[x, y] = DesiredValue
+
+
+BucketTool([50, 50], 3)
+plt.imshow(colorLayer)
+plt.show()
