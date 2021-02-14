6addcde92405cc7a865c18dd6230c0b4871e19ff
mario <mario.hanser@web.de>
2021-02-14 11:36:46 +0100

    bessere input validation

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -39,60 +39,91 @@ function joinGame(event) {
     event.preventDefault();
     
     // get the game code and clear the inut field
-    var game_code_input = document.getElementById("game-code-input");
-    var game_code = game_code_input.value;
-    
-    // TODO: game code validation on server side
+    const game_code_input = document.getElementById("game-code-input");
+    const game_code = game_code_input.value;
     
     // get the username and clear the inut field
-    var name_input = document.getElementById("name-input");
-    var user_name = name_input.value;
+    const name_input = document.getElementById("name-input");
+    const user_name = name_input.value;
     
     // user name validation
-    if (user_name.length < 3 || user_name.lengt > 25) {
-        if (game_code.length == 6) {
-            const game_code_input_box = document.getElementById("game-code-input-box");
-            const invalid_game_code = document.getElementById("invalid-game-code-input");
-            game_code_input_box.removeChild(invalid_game_code);
-            game_code_input.classList.remove("is-invalid");
-        }        
+    if ((user_name.length < 3 || user_name.lengt > 25) & game_code.length !== 6) {
+ 
+        // make name input red, if it is not already red
         if (!document.getElementById("invalid-name-input")) {
             name_input.classList.add("is-invalid");
             
-            var invalid_name_text = document.createTextNode("3-25 characters");
-            var label = document.createElement("label");
+            const invalid_name_text = document.createTextNode("3-25 characters");
+            const label = document.createElement("label");
             label.className = "form-label text-danger";
             label.id = "invalid-name-input";
             label.appendChild(invalid_name_text);
             
-            var name_input_box = document.getElementById("name-input-box");
+            const name_input_box = document.getElementById("name-input-box");
             name_input_box.appendChild(label);
         }
+        
+        // make game code input red, if it isnt
+        if (!document.getElementById("invalid-game-code-input")) {
+            game_code_input.classList.add("is-invalid");
+            
+            const invalid_name_text = document.createTextNode("invalid game code");
+            const label = document.createElement("label");
+            label.className = "form-label text-danger";
+            label.id = "invalid-game-code-input";
+            label.appendChild(invalid_name_text);
+            
+            const game_code_input_box = document.getElementById("game-code-input-box");
+            game_code_input_box.appendChild(label);
+        }           
     } else if (game_code.length !== 6) {
+        
+        console.log(document.getElementById("invalid-name-input"));
+        // make name input normal if it is red and has a valid entry
         if (document.getElementById("invalid-name-input")) {
             const name_input_box = document.getElementById("name-input-box");
             const invalid_name = document.getElementById("invalid-name-input");
             name_input_box.removeChild(invalid_name);
             name_input.classList.remove("is-invalid");
         }
+        
+        // make came code input red
         if (!document.getElementById("invalid-game-code-input")) {
             game_code_input.classList.add("is-invalid");
             
-            var invalid_name_text = document.createTextNode("invalid game code");
-            var label = document.createElement("label");
+            const invalid_name_text = document.createTextNode("invalid game code");
+            const label = document.createElement("label");
             label.className = "form-label text-danger";
             label.id = "invalid-game-code-input";
             label.appendChild(invalid_name_text);
             
-            var game_code_input_box = document.getElementById("game-code-input-box");
+            const game_code_input_box = document.getElementById("game-code-input-box");
             game_code_input_box.appendChild(label);
-        }        
+        }
+    } else if (user_name.length < 3 || user_name.lengt > 25) {
+        
+        // make game code input normal if it is red and has valid entry
+        if (document.getElementById("invalid-game-code-input")) {
+            const game_code_input_box = document.getElementById("game-code-input-box");
+            const invalid_game_code = document.getElementById("invalid-game-code-input");
+            game_code_input_box.removeChild(invalid_game_code);
+            game_code_input.classList.remove("is-invalid");
+        }
+        
+        // make name input red, if it is not already red
+        if (!document.getElementById("invalid-name-input")) {
+            name_input.classList.add("is-invalid");
+            
+            const invalid_name_text = document.createTextNode("3-25 characters");
+            const label = document.createElement("label");
+            label.className = "form-label text-danger";
+            label.id = "invalid-name-input";
+            label.appendChild(invalid_name_text);
+            
+            const name_input_box = document.getElementById("name-input-box");
+            name_input_box.appendChild(label);
+        }
     } else {
         window.location.href = this.href + "?user-name=" + user_name + "&game-code=" + game_code;
     }
 }
-
-
-
-
-
