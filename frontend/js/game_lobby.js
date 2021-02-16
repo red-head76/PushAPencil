501f7a5e7fd44a86068962c8e65bf6d94560bebb
red-head76 <reddy.uni@t-online.de>
2021-02-16 19:01:46 +0100

    style errors and minor bug fixes

--- frontend/js/game_lobby.js
+++ frontend/js/game_lobby.js
@@ -7,11 +7,11 @@ const user_name = url.searchParams.get("user-name");
 var game_code = url.searchParams.get("game-code");
 
 // divs to switch between for differen game states
-const game_lobby_div = document.getElementById("game-lobby-div");        
+const game_lobby_div = document.getElementById("game-lobby-div");
 const draw_div = document.getElementById("draw-div");
 const starting_phrase_div = document.getElementById("starting-phrase-div");
 const describe_div = document.getElementById("describe-div");
-        
+
 // to handle disconnects different if the game has game has started
 var is_waiting = false;
 var game_state = "lobby";  // one of lobby, start, draw, describe
@@ -29,9 +29,9 @@ socket.on("new game lobby", function(new_game_code) {
 
 socket.on("users in room", function(users) {
     users.forEach(user => createUserNameInList(user.user_name));
-    
+
     // append the user itself after all users that where already here
-    createUserNameInList(user_name);    
+    createUserNameInList(user_name);
 });
 
 socket.on("join game lobby", function(user_name) {
@@ -55,17 +55,17 @@ socket.on("user leave", function(remaining_users) {
 // user lobby functions
 // ________________________________________________________________________________________________
 function load() {
-    
+
     // TODO load screens for different game states, but persistend socket.id required
     if (game_state === "lobby") {
-        
+
         makeGameLobbyScreen ()
-        
+
         // try to get the game code from url to see if the player wants to create or join a room
         if (game_code) {
             // user clicked on join room and passed therfore a game code
             socket.emit("join game lobby", game_code, user_name);
-            
+
             // write game code to span
             const game_code_span = document.getElementById("game-code");
             const code = document.createTextNode(game_code);
@@ -82,7 +82,7 @@ function createUserNameInList(user_name) {
     const user_list = document.getElementById("users");
     const new_user = document.createTextNode(user_name);
     const new_user_entry = document.createElement("li");
-    
+
     new_user_entry.className = "list-group-item";
     new_user_entry.appendChild(new_user);
     user_list.appendChild(new_user_entry);
@@ -129,7 +129,7 @@ function startGame() {
 }
 
 function startWithDrawing() {
-    
+
     const starting_phrase = document.getElementById("starting-phrase-input").value;
     socket.emit("starting phrase", starting_phrase, game_code);
 
@@ -144,13 +144,13 @@ function startWithDrawing() {
 }
 
 function continueWithDrawing() {
-    
+
     if (game_state === "lobby") {
         const starting_phrase = document.getElementById("starting-phrase-input").value;
         // TODO input validation 5 < length < 50???, only a-z, 1-9, and ' ', (&/()[]) ???
         socket.emit("starting phrase", starting_phrase);
     }
-    
+
     // TODO counter for game length and Game end
     if (tasks.length > 0) {
         makeDrawScreen();
@@ -163,7 +163,7 @@ function continueWithDrawing() {
 }
 
 function continueWithDescribing() {
-    
+
     if (tasks.length > 0) {
         makeDescribeScreen();
         game_state = "describe";
@@ -193,11 +193,11 @@ function makeDrawScreen(phrase) {
     starting_phrase_div.style.display = "none";
     draw_div.style.display = "";
     game_lobby_div.style.display = "none";
-    
+
     const to_draw = document.getElementById("to-draw");
     const text_node = document.createTextNode(phrase);
     to_draw.appendChild(text_node);
-    
+
     initDrawingTool();
 }
 
@@ -223,15 +223,14 @@ var ctx = canvas.getContext('2d');
 
 // last known position
 var pos = { x: 0, y: 0 };
-var rect;
+var rect = canvas.getBoundingClientRect();
 var offset_x;
 var offset_y;
 
 var mode = 'pencil'; // pencil, fill
-var background_color = '#000000'; // Black, not really true, but background filling tool works then
+var background_color = '#ffffff'; // White
 var last_color = '#000000';     // Black
 
-// window.addEventListener('load', load);
 window.addEventListener('resize', resize);
 canvas.addEventListener('mousemove', drawLine);
 canvas.addEventListener('mousedown', beginLine);
@@ -244,19 +243,15 @@ function clearAll() {
     ctx.lineWidth = "5";
     ctx.lineCap = "round";
     mode = 'pencil'; // pencil, fill
-  
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    background_color = "white";
-    
+    background_color = 'white';
     ctx.strokeStyle = "black";
     last_color = 'black';
-
     pencil.style.fill = 'gray';
     rubber.style.fill = 'white';
     background.style.fill = 'white';
     fill.style.fill = 'white';
-
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     show_color.style.fill = 'black';
 }
 
@@ -273,7 +268,6 @@ function initDrawingTool() {
     ctx.lineWidth = "5";
     ctx.strokeStyle = "black";
     ctx.lineCap = "round";
-    
     rect = canvas.getBoundingClientRect();
     offset_x = rect.left;
     offset_y = rect.top;
@@ -286,20 +280,20 @@ function initDrawingTool() {
     show_color.style.fill = 'black';
 }
 
-function getX(e) { return (e.clientX - Math.floor(offset_x)); }
-function getY(e) { return (e.clientY - Math.floor(offset_y)); }
 
 function fillBackground() {
     // Fills the canvas at every pixel that matches the old background color
     colorLayer = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var old_Bkg_Color_RGB = HEXtoRGB(background_color);
+    const old_Bkg_Color = HEXtoRGBA(background_color);
+    console.log("old background:", old_Bkg_Color);
     background_color = ctx.strokeStyle;
-    var new_Bkg_Color_RGB = HEXtoRGB(background_color);
+    const new_Bkg_Color = HEXtoRGBA(background_color);
+    console.log("new background:", new_Bkg_Color);
 
     for (var x = 0; x < canvas.width; x++) {
         for (var y = 0; y < canvas.height; y++) {
-            if (checkColorMatch(colorLayer, x, y, old_Bkg_Color_RGB)) {
-                colorPixel(colorLayer, x, y, new_Bkg_Color_RGB);
+            if (checkColorMatch(colorLayer, x, y, old_Bkg_Color)) {
+                colorPixel(colorLayer, x, y, new_Bkg_Color);
             }
         }
     }
@@ -308,7 +302,7 @@ function fillBackground() {
 
 // new position from mouse event
 function beginLine(e) {
-    if (mode === 'pencil' || mode === 'rubber') {
+    if (mode == 'pencil' || mode == 'rubber') {
         if (e.button == 1) {
             fillBackground()
         } else {
@@ -318,14 +312,13 @@ function beginLine(e) {
             ctx.beginPath();
             ctx.moveTo(pos.x, pos.y);
         }
-    } else if (mode === 'fill') {
+    } else if (mode == 'fill') {
         bucketTool(e);
     }
 }
 
 function drawLine(e) {
-    console.log(mode);
-    if (mode === 'pencil' || mode === 'rubber') {
+    if (mode == 'pencil' || mode == 'rubber') {
         if (e.buttons !== 1) return;
 
         pos.x = getX(e);
@@ -338,7 +331,7 @@ function drawLine(e) {
 }
 
 function endLine(e) {
-    if (mode === 'pencil' || mode === 'rubber') {
+    if (mode == 'pencil' || mode == 'rubber') {
         if (e.button !== 1) {
             pos.x = getX(e);
             pos.y = getY(e);
@@ -349,21 +342,29 @@ function endLine(e) {
     }
 }
 
+function tweezerTool(e) {
+    const colorLayer = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const startColor = getPixelColor(colorLayer, getX(e), getY(e));
+    console.log("R:", startColor[0]);
+    console.log("G:", startColor[1]);
+    console.log("B:", startColor[2]);
+    console.log("Alpha:", startColor[3]);
+    console.log(Math.min.apply(Math, startColor))
+}
+
+
 // bucket tool (fill tool) from
 // http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
 function bucketTool(e) {
     var colorLayer = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var startX = getX(e);
-    var startY = getY(e);
+    const startX = getX(e);
+    const startY = getY(e);
     var pixelStack = [[startX, startY]];
-    var startPixelPos = calcPixelPos(startX, startY)
-    var StartColor = [colorLayer.data[startPixelPos], colorLayer.data[startPixelPos+1],
-                  colorLayer.data[startPixelPos+2], colorLayer.data[startPixelPos+3]];
-    var FillColor = HEXtoRGB(ctx.strokeStyle);
+    const StartColor = getPixelColor(colorLayer, startX, startY);
+    const FillColor = HEXtoRGBA(ctx.strokeStyle);
     var newPos, x, y, reachLeft, reachRight;
 
-    if (StartColor[0] !== FillColor[0] || StartColor[1] !== FillColor[1] ||
-        StartColor[2] !== FillColor[2]) {
+    if (!equals(StartColor, FillColor)) {
         while (pixelStack.length) {
             newPos = pixelStack.pop();
             x = newPos[0];
@@ -501,34 +502,56 @@ big_pencil.addEventListener('click', function(event) {
 
 // _____________________________________________________________________________
 // Helper functions
+
+// Array comparison
+const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
+// get position with canvas offset
+function getX(e) { return (e.clientX - Math.floor(offset_x)); }
+function getY(e) { return (e.clientY - Math.floor(offset_y)); }
+
+
 // converting x, y coordinates into 1d position
-function calcPixelPos(x, y) {
+function getPixelPos(x, y) {
     return ((y * canvas.width + x) * 4);
+}
+
+// Return color (RGBA) at pixel position
+function getPixelColor(colorLayer, x, y) {
+    const pixelPos = getPixelPos(x, y);
+    const color = [colorLayer.data[pixelPos], colorLayer.data[pixelPos+1],
+                   colorLayer.data[pixelPos+2], colorLayer.data[pixelPos+3]];
+    if (equals(color, [0, 0, 0, 0])) {
+        return [255, 255, 255, 255];
+    } else {
+        return color;
+    }
 }
 
 // checks if the color at x, y matches the desired color
 function checkColorMatch(colorLayer, x, y, matchColor) {
-    pixelPos = calcPixelPos(x, y);
-    return (colorLayer.data[pixelPos] == matchColor[0] &&
-            colorLayer.data[pixelPos + 1]== matchColor[1] &&
-            colorLayer.data[pixelPos + 2] == matchColor[2]);
+    pixelColor = getPixelColor(colorLayer, x, y);
+    return equals(pixelColor, matchColor);
+    // return (colorLayer.data[pixelPos] == matchColor[0] &&
+    //         colorLayer.data[pixelPos + 1]== matchColor[1] &&
+    //         colorLayer.data[pixelPos + 2] == matchColor[2]);
 }
 
-// transforms hex color (string) into RGB (list) values
-function HEXtoRGB(hex) {
+// transforms hex color (string) into RGBA (list) values
+function HEXtoRGBA(hex) {
     hex = hex.replace(/#/g, '');
     if (hex.length === 3) {
         hex = hex.split('').map(function (hex) {
             return hex + hex;
         }).join('');
     }
-    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+    const result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
     if (result) {
-        var red = parseInt(result[1], 16);
-        var green = parseInt(result[2], 16);
-        var blue = parseInt(result[3], 16);
+        const red = parseInt(result[1], 16);
+        const green = parseInt(result[2], 16);
+        const blue = parseInt(result[3], 16);
 
-        return [red, green, blue];
+        return [red, green, blue, 255]; // just return full alpha (255) everywhere
     } else {
         return null;
     }
@@ -536,85 +559,9 @@ function HEXtoRGB(hex) {
 
 // changes the value (color) in a colorLayer to a desired color
 function colorPixel(colorLayer, x, y, newColor) {
-    pixelPos = calcPixelPos(x, y);
+    pixelPos = getPixelPos(x, y);
     colorLayer.data[pixelPos] = newColor[0];
     colorLayer.data[pixelPos+1] = newColor[1];
     colorLayer.data[pixelPos+2] = newColor[2];
     colorLayer.data[pixelPos+3] = 255;
-}
-
-
-// _____________________________________________________________________________
-// Alternative bucket tool function
-function bucketTool2(e) {
-    var canvasWidth = canvas.width;
-    var canvasHeight = canvas.height;
-    colorLayer = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-    startX = getX(e);
-    startY = getY(e);
-    pixelStack = [[startX, startY]];
-
-    function pixelPos(x, y) {   // converting x, y coordinates into 1d position
-        return ((y * canvasWidth + x) * 4);
-    }
-
-    var StartR = colorLayer.data[pixelPos(startX, startY)];
-    var StartG = colorLayer.data[pixelPos(startX, startY) + 1];
-    var StartB = colorLayer.data[pixelPos(startX, startY) + 2];
-    var StartA = colorLayer.data[pixelPos(startX, startY) + 3];
-    var FillColorRGB = HEXtoRGB(ctx.strokeStyle);
-    var newPos, x, y;
-
-    while (pixelStack.length) {
-        newPos = pixelStack.pop();
-        x = newPos[0];
-        y = newPos[1];
-
-        if (matchStartColor(pixelPos(x, y)) && (StartR !== FillColorRGB[0] ||
-            StartG !== FillColorRGB[1] || StartB !== FillColorRGB[2])) {
-            colorPixel(pixelPos(x, y));
-
-            pixelStack.push([x, y + 1]); // below
-            pixelStack.push([x, y - 1]); // above
-            pixelStack.push([x + 1, y]); // right
-            pixelStack.push([x - 1, y]); // left
-        }
-    }
-    ctx.putImageData(colorLayer, 0, 0);
-
-    function matchStartColor(pixelPos) {
-        var r = colorLayer.data[pixelPos];
-        var g = colorLayer.data[pixelPos + 1];
-        var b = colorLayer.data[pixelPos + 2];
-        var a = colorLayer.data[pixelPos + 3];
-
-        return (r == StartR && g == StartG && b == StartB && a==StartA);
-    }
-
-    function HEXtoRGB(hex) {
-        hex = hex.replace(/#/g, '');
-        if (hex.length === 3) {
-            hex = hex.split('').map(function (hex) {
-                return hex + hex;
-            }).join('');
-        }
-        var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
-        if (result) {
-            var red = parseInt(result[1], 16);
-            var green = parseInt(result[2], 16);
-            var blue = parseInt(result[3], 16);
-
-            return [red, green, blue];
-        } else {
-            return null;
-        }
-    }
-
-    function colorPixel(pixelPos) {
-        var FillColorRGB = HEXtoRGB(ctx.strokeStyle);
-        colorLayer.data[pixelPos] = FillColorRGB[0];
-        colorLayer.data[pixelPos+1] = FillColorRGB[1];
-        colorLayer.data[pixelPos+2] = FillColorRGB[2];
-        colorLayer.data[pixelPos+3] = 255;
-    }
 }
